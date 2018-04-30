8b090efdfe98a5b36bd183ec2a215d0af78a0bf9
gurpreetkaur162 <gurpreetheer162@gmail.com>
2018-04-30 12:59:52 +0530

    30 april

--- app/src/main/java/com/example/admin/myapplication/ProfileFragment.java
+++ app/src/main/java/com/example/admin/myapplication/ProfileFragment.java
@@ -1,11 +1,13 @@
 package com.example.admin.myapplication;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
+    ImageView img;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -23,7 +26,18 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
+       View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
+       img = v.findViewById(R.id.back);
+
+       img.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent i = new Intent(getContext(), Home.class );
+               startActivity(i);
+           }
+       });
+
+        return v;
+    }
 }
